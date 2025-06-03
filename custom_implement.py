3c095628c8029b0ff67d780cf952ddf129895542
ChangSik Woo <jasoncswoo@gmail.com>
2025-06-03 17:22:51 +0900

    Fix : embed_dim --> self.embed_dim

--- custom_implement.py
+++ custom_implement.py
@@ -9,6 +9,7 @@ class PatchEmbedding(nn.Module):
         super().__init__()
         self.img_size = img_size
         self.patch_size = patch_size
+        self.embed_dim = embed_dim
         self.num_patch = (img_size // patch_size) ** 2
         
         self.projection = nn.Conv2d(in_channels=in_channels, out_channels=embed_dim, kernel_size=patch_size, stride = patch_size) # (bs, C, H, W) --> (bs, embed_dim, H, W)
@@ -16,7 +17,7 @@ class PatchEmbedding(nn.Module):
 
     def forward(self, x):
         x = self.projection(x)
-        x = x.reshape(x.shape[0], -1, embed_dim) # (bs, embed_dim, H, W) --> (bs, (HW), embed_dim)
+        x = x.reshape(x.shape[0], -1, self.embed_dim) # (bs, embed_dim, H, W) --> (bs, (HW), embed_dim)
         return x
 
 
