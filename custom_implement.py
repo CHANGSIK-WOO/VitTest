15ebe54279c7879d848bb02388a5b0c30546f370
ChangSik Woo <jasoncswoo@gmail.com>
2025-06-03 17:50:27 +0900

    Fix : change

--- custom_implement.py
+++ custom_implement.py
@@ -7,25 +7,28 @@ import einops
 class PatchEmbedding(nn.Module):
     def __init__(self, img_size : int, patch_size : int, in_channels : int, embed_dim : int = 768):
         super().__init__()
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.embed_dim = embed_dim
-        self.num_patch = (img_size // patch_size) ** 2
+        self.img_size = img_size # 256¡¿256
+        self.patch_size = patch_size # 16¡¿16
+        self.embed_dim = embed_dim # 768
+        self.num_patch = (img_size // patch_size) ** 2 # 16
         
-        self.projection = nn.Conv2d(in_channels=in_channels, out_channels=embed_dim, kernel_size=patch_size, stride = patch_size) # (bs, C, H, W) --> (bs, embed_dim, H, W)
+        self.projection = nn.Conv2d(in_channels=in_channels, out_channels=embed_dim, kernel_size=patch_size, stride = patch_size) 
+        # (bs, C, H, W) --> (bs, embed_dim, H', W')
+        # (bs, 3, 256, 256) --> (bs, 768, 16, 16)
 
 
     def forward(self, x):
-        print(x.shape)
-        x = self.projection(x)
-        print(x.shape)
-        x = x.reshape(x.shape[0], -1, self.embed_dim) # (bs, embed_dim, H, W) --> (bs, (HW), embed_dim)
+        print(x.shape) # (bs, 3, 256, 256)
+        x = self.projection(x) 
+        print(x.shape) # (bs, 3, 256, 256) --> (bs, 768, 16, 16)
+        x = einops.rearrange(x, "b c h w -> b (h w) c") # (bs, 768, 16, 16) --> (bs, 256, 768)
         return x
 
 
 x = torch.rand([32, 3, 256, 256])
 print(x.shape)
-pe = PatchEmbedding(x.shape[2], 16, x.shape[1], 768)
+bs, c, h, w = x.shape
+pe = PatchEmbedding(img_size = h, patch_size = 16, in_channels = c, embed_dim = 768)
 x = pe(x)
 print(x.shape)
 
